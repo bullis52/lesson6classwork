87617dc0c3d11908d2776263c23f35d88b4655c5
1 <tawers6103@gmail.com>
2021-07-27 13:05:36 +0300

    js

--- main.js
+++ main.js
@@ -2,3 +2,30 @@
 //Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 //https://jsonplaceholder.typicode.com/posts
 //    зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(value => value.json())
+    .then(posts => {
+
+        for (const post of posts) {
+                let userDiv = document.createElement('div');
+                let btn = document.createElement('button');
+                userDiv.append(btn);
+                btn.innerText = 'press'
+                let h2 = document.createElement('h2')
+                let h3 = document.createElement('h3')
+                let p = document.createElement('p')
+                let span = document.createElement('span')
+                btn.onclick = function (){
+                   //userDiv.append(`${post.id}`,`${post.userId}`,`${post.title}`,`${post.body}`)
+                    userDiv.append(h2)
+                    userDiv.append(h3)
+                    userDiv.append(p)
+                    userDiv.append(span)
+                    h2.innerText = `${post.id}`
+                    h3.innerText = `${post.userId}`
+                    p.innerText = `${post.title}`
+                    span.innerText = `${post.body}`
+                }
+                let target = document.getElementById('target');
+                target.append(userDiv);
+        }})
